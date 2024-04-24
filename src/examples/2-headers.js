b415dca312e079579d3b9979bfa51e48ee7a2862
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-24 23:05:52 +0530

    Random Dad Joke axios call with headers

--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -1,22 +1,51 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/";
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [loading, setLoading] = useState(false);
+  const [joke, setJoke] = useState("random dad joke");
+  const [error, setError] = useState(null);
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    try {
+      setLoading(true);
+      const response = await axios(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      setLoading(false);
+      console.log(response);
+      setJoke(response.data.joke);
+    } catch (error) {
+      console.log("ERROR:");
+      console.log(error.message);
+      setLoading(false);
+      setError(error.message);
+    }
   };
 
+  if (loading) {
+  }
+
+  if (error) {
+    return <h3>{error}</h3>;
+  }
+
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
-        random joke
+    <section className="section text-center">
+      <button
+        className="btn"
+        onClick={fetchDadJoke}
+      >
+        get a random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+
+      {loading ? <h3>loading...</h3> : <p className="dad-joke">{joke}</p>}
     </section>
   );
 };

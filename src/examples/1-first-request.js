2a1a4ff2f8860558f84ad6e408f636f80953ada3
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-24 22:42:01 +0530

    axios default is get request

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://www.course-api.com/react-store-products";
+
+// Axios treats all the errors as errors unless fetch!
 
 const FirstRequest = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      setProducts(response.data);
+      setIsLoading(false);
+      console.log(response);
+    } catch (error) {
+      const errorMessage = error.response.data.msg;
+      setIsLoading(false);
+      setError(errorMessage);
+    }
+  };
+
   useEffect(() => {
-    console.log('first axios request');
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  if (isLoading) {
+    return <h1>LOADING ...</h1>;
+  }
+
+  if (error) {
+    return <h3 style={{ color: "red" }}>{error}</h3>;
+  }
+  return <h2 className="text-center">Total Products: {products.length}</h2>;
 };
 export default FirstRequest;

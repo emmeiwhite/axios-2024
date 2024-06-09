f7ec58a0435b5923ded6294f675798515df40a19
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-06-09 15:52:57 +0530

    Made successful get request with axios

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,10 +1,25 @@
 import { useEffect } from 'react'
+import axios from 'axios'
 
 // limit, if 429 wait for 15 min and try again
 const url = 'https://www.course-api.com/react-store-products'
 
-// Axios treats all the errors as errors unless fetch!
+// Axios handles all HTTP errors consistently, unlike fetch which requires manual handling for certain errors.
+
 const FirstRequest = () => {
+  const getProducts = async () => {
+    try {
+      const { data } = await axios(url)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return <h2 className="text-center">First Request</h2>
 }
 export default FirstRequest
